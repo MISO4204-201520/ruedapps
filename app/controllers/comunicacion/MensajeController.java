96ec3e883cfb058463aa860711e1b6deb01c2777
franciscoluisrv <franciscoluisrv@yahoo.com>
2015-10-17 12:45:49 -0500

    Simplificar mensajes

--- app/controllers/comunicacion/MensajeController.java
+++ app/controllers/comunicacion/MensajeController.java
@@ -17,6 +17,70 @@ import java.util.List;
  * Created by franciscoluisrv on 10/03/2015.
  */
 public class MensajeController extends Controller {
+    public WebSocket<JsonNode> MensajeSocket() {
+        return WebSocket.whenReady((in, out) -> {
+            // Recibir mensaje desde forma Angular
+            in.onMessage(node -> {
+                // Crear mensaje desde nodo Json
+                Mensaje mensaje = CrearMensaje(node);
+
+                if (mensaje != null) {
+                    // Guardar mensaje enviado
+                    mensaje.save();
+
+                    // Reenviar mensaje que llegó
+                    out.write(node);
+                }
+            });
+
+            // Registrar cierre de socket
+            in.onClose(() -> System.out.println("Cerrado mensaje socket"));
+        });
+    }
+
+    private Mensaje CrearMensaje(JsonNode node) {
+        Ciclista ciclistaFrom = BuscarCiclistaPorCorreo(node.get("remitente").asText());
+        Ciclista ciclistaTo = BuscarCiclistaPorCorreo(node.get("destinatario").asText());
+        Mensaje nuevoMensaje = null;
+
+        if (ciclistaFrom != null && ciclistaTo != null) {
+            nuevoMensaje = new Mensaje();
+            nuevoMensaje.fechaEnvio = new Date();
+            nuevoMensaje.remitente = ciclistaFrom;
+            nuevoMensaje.destinatario = ciclistaTo;
+            nuevoMensaje.texto = node.get("mensaje").asText();
+        }
+
+        return nuevoMensaje;
+    }
+
+    private Mensaje CrearMensaje(MensajeDTO dto) {
+        Ciclista ciclistaFrom = BuscarCiclistaPorCorreo(dto.remitente);
+        Ciclista ciclistaTo = BuscarCiclistaPorCorreo(dto.destinatario);
+        Mensaje nuevoMensaje = null;
+
+        if (ciclistaFrom != null && ciclistaTo != null) {
+            nuevoMensaje = new Mensaje();
+            nuevoMensaje.fechaEnvio = new Date();
+            nuevoMensaje.remitente = ciclistaFrom;
+            nuevoMensaje.destinatario = ciclistaTo;
+            nuevoMensaje.texto = dto.mensaje;
+        }
+
+        return nuevoMensaje;
+    }
+
+
+    private Ciclista BuscarCiclistaPorCorreo(String correo) {
+        List<Ciclista> ciclistas = Ciclista.findCiclista.where().eq("correoElectronico", correo).findList();
+        Ciclista ciclista = null;
+
+        if (ciclistas != null && ciclistas.size() == 1) {
+            ciclista = ciclistas.get(0);
+        }
+
+        return ciclista;
+    }
 
     public Result MensajePost() {
         // Recibir mensaje desde forma Scala
@@ -33,48 +97,8 @@ public class MensajeController extends Controller {
         }
     }
 
-    public WebSocket<JsonNode> MensajeSocket() {
-        return WebSocket.whenReady((in, out) -> {
-            // Recibir mensaje desde forma Angular
-            in.onMessage(node -> {
-                // Crear dto desde nodo Json
-                MensajeDTO dto = new MensajeDTO();
-                dto.remitente = node.get("remitente").asText();
-                dto.destinatario = node.get("destinatario").asText();
-                dto.mensaje = node.get("mensaje").asText();
-                Mensaje mensaje = CrearMensaje(dto);
-
-                if (mensaje != null) {
-                    // Guardar mensaje cuando llega
-                    mensaje.save();
-
-                    // Reenviar mensaje que llegó
-                    out.write(node);
-                }
-            });
-
-            // Registrar cierre de socket
-            in.onClose(() -> System.out.println("Cerrado mensaje socket"));
-        });
-    }
-
-    private Mensaje CrearMensaje(MensajeDTO dto) {
-        List<Ciclista> ciclistaFrom = Ciclista.findCiclista.where().eq("correoElectronico", dto.remitente).findList();
-        List<Ciclista> ciclistaTo = Ciclista.findCiclista.where().eq("correoElectronico", dto.destinatario).findList();
-        Mensaje nuevoMensaje = null;
-
-        if (ciclistaFrom != null && ciclistaFrom.size() == 1 && ciclistaTo != null && ciclistaTo.size() == 1) {
-            nuevoMensaje = new Mensaje();
-            nuevoMensaje.fechaEnvio = new Date();
-            nuevoMensaje.remitente = ciclistaFrom.get(0);
-            nuevoMensaje.destinatario = ciclistaTo.get(0);
-            nuevoMensaje.texto = dto.mensaje;
-        }
-
-        return nuevoMensaje;
-    }
-
     public Result Mensaje() {
+        // Respuesta para forma Scala
         return ok("");
     }
 }
