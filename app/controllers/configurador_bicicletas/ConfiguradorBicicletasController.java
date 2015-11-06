e5a1ec0c8558b4258b789d7537a4557722bdcaa9
Lina <lina.m8a@gmail.com>
2015-11-06 12:23:03 -0500

    Se crean tablas bicicleta, accesorios_bicicleta y bicicleta_usuario en la bd. Se crea el modelo Bicicleta.

---
+++ app/controllers/configurador_bicicletas/ConfiguradorBicicletasController.java
@@ -0,0 +1,9 @@
+package controllers.configurador_bicicletas;
+
+import play.mvc.Controller;
+
+/**
+ * Created by Lina8a on 06/11/2015.
+ */
+public class ConfiguradorBicicletasController extends Controller {
+}
