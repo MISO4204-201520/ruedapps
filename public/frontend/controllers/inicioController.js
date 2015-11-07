b4ec5009da8ea52b898f19ef7680a444b171b1cc
franciscoluisrv <franciscoluisrv@yahoo.com>
2015-11-07 11:56:53 -0500

    Div para notificaciones

--- public/frontend/controllers/inicioController.js
+++ public/frontend/controllers/inicioController.js
@@ -4,48 +4,12 @@
 
 ruedapp.controller('inicioController', ['$scope', '$rootScope', '$location', '$http',
     function ($scope, $rootScope, $location, $http) {
-        /**
-         * Obtención de amigos y usuarios de la plataforma.
+        /*
+         Mostrar notificaciones
          */
         $(function () {
-            var get = {
-                method: 'GET',
-                url: '/ciclista/' + $rootScope.globals.currentUser.userId
-            };
-
-            $http(get).success(function (data) {
-                console.log("Obtuvo usuario");
-                console.log("data: " + data);
-                $scope.userGlobalId = data;
-
-                var get1 = {
-                    method: 'GET',
-                    url: '/ciclista/' + $scope.userGlobalId + '/amigos'
-                };
-                $http(get1).success(function (data) {
-                    $scope.amigos = data;
-
-                }).error(function (data) {
-                    console.log("Error amigos.");
-                    console.log("data: " + data);
-                });
-
-            }).error(function (data) {
-                console.log("Error obtención id.");
-                console.log("data: " + data);
-            });
+            $scope.notificationType = "INFO";
+            $scope.notificationText = "Some content";
+            $scope.showNotification = false;
         });
-
-        var planearRecorrido = function () {
-            window.location.replace('#/recorridos');
-        };
-
-        var enviarMensaje = function () {
-            window.location.replace('#/mensaje');
-        };
-
-        var agregarAmigo = function () {
-            window.location.replace('#/amigos');
-        };
-
     }]);
