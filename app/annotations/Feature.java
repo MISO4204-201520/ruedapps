1633ad84d7a98a7d6fd83bb478d8b431f06655a2
Lina <lina.m8a@gmail.com>
2015-11-19 22:19:06 -0500

    Se agrega anotación Feature.

---
+++ app/annotations/Feature.java
@@ -0,0 +1,15 @@
+package annotations;
+
+import java.lang.annotation.Target;
+
+/**
+ * Created by Lina8a on 19/11/2015.
+ */
+public @interface Feature {
+
+    /**
+     * Nombre del identificador del feature.
+     * @return nombre
+     */
+    String nombre();
+}
