6e31a532a5097aadffac480ad486b34e475cc0ad
Lina <lina.m8a@gmail.com>
2015-11-23 00:20:24 -0500

    Se elimina anotación del modelo desplazamiento.

--- app/models/ruta/desplazamiento/DesplazamientoGrupal.java
+++ app/models/ruta/desplazamiento/DesplazamientoGrupal.java
@@ -13,7 +13,6 @@ import java.util.List;
 /**
  * Created by Juan on 11/14/2015.
  */
-@Feature(nombre = "Grupal")
 public class DesplazamientoGrupal extends DesplazamientoDecorator {
 
     public List<Ciclista> participantes;
